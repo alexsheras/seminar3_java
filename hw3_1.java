b8e3957f5504c40f6c0ac5dc2f7ed5805d80eb92
ALEX SHER <sheras85@mail.com>
2023-04-19 15:52:27 +0300

    задача1

---
+++ hw3_1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class hw3_1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        array = lib.FillRandomArray(10);
+        lib.PrintArray(array);
+        DeleteEvenNumber(array);
+        lib.PrintArray(array);
+    }
+   public static void DeleteEvenNumber(ArrayList<Integer> list){
+        int i = 0;
+        while (i < list.size()){
+            if(list.get(i) % 2 == 0) list.remove(i);
+            else i++;
+        }
+    }
+}
